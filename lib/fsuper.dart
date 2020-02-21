ae12840f91a46a4ce95b825f5ae805f4d94e4b94
纽特 <coorchice.cb@alibaba-inc.com>
2020-02-21 11:32:45 +0800

    - fix#0.1.3#优化渲染效率 - other#0.1.3#优化代码注释

--- lib/fsuper.dart
+++ lib/fsuper.dart
@@ -1,57 +1,195 @@
 import 'package:flutter/material.dart';
 
+/// 圆角风格。
+/// [round] - 圆角
+/// [bevel] - 斜角
+///
+/// Rounded corner style.
+/// [round]-rounded corners
+/// [bevel]-beveled corners
 enum CornerStyle {
   round,
   bevel,
 }
 
-enum AlignTextMode {
-  left,
-  top,
-  right,
-  bottom,
-}
-
+/// FSuper 是一个强大的组件，能够支持富文本、圆角、边框、图片、小红点、以及同时设置多达两个子组件，且控制它们的相对位置。
+/// FSuper 能够帮助开发者快速舒适的构建复杂视图。
+///
+/// FSuper is a powerful component that can support rich text, rounded corners, borders, pictures, small red dots, and set up to two sub-components at the same time, and control their relative positions.
+/// FSuper can help developers build complex views quickly and comfortably.
 class FSuper extends StatefulWidget {
+
+  /// 宽。null 将会自适应文字大小。设置 double.infinity 将会充满父容器。
+  ///
+  /// Width，null will adapt the text size. Setting double.infinity will fill the parent container.
   double width;
+  /// 宽。null 将会自适应文字大小。
+  ///
+  /// Height， null will adapt the text size.
   double height;
+  /// 组件最大宽度。
+  ///
+  /// Maximum component width.
   double maxWidth;
+  /// 组件最大高度
+  ///
+  /// Maximum component height
   double maxHeight;
+  /// 文字内容
+  ///
+  /// Text content
   String text;
+  /// 文字颜色
+  ///
+  /// Text color
   Color textColor;
+  /// 文字字号
+  ///
+  /// Text size
   double textSize;
+  /// 文字风格。详见 [TextStyle]
+  ///
+  /// Text style. See [TextStyle] for details
   FontStyle textStyle;
+  /// 文字宽度。默认 [FontWeight.normal]
+  ///
+  /// Text width. Default [FontWeight.normal]
   FontWeight textWeight;
+  /// 文字整体相对位置。详见 [Alignment]
+  ///
+  /// The overall relative position of the text. See [Alignment] for details
   Alignment textAlignment;
+  /// 文字对齐方式。详见 [Align]
+  ///
+  /// Text alignment. See [Align] for details
   TextAlign textAlign;
+  /// 富文本内容。详见 [TextSpan] 。
+  /// [TextSpan] 元素默认会使用 [textColor]，[textSize]，[textStyle]，[textWeight] 的配置。
+  ///
+  /// Rich text content. See [TextSpan] for more details.
+  /// The [TextSpan] element uses the configuration of [textColor], [textSize], [textStyle], [textWeight] by default.
   List<TextSpan> spans;
+  /// 点击监听回调
+  ///
+  /// Click listen callback
   GestureTapCallback onClick;
+  /// 组件背景颜色。
+  ///
+  /// Component background color
   Color backgroundColor;
+  /// 设置组件渐变色背景。会覆盖 [backgroundColor]
+  /// 你可选择 [LinearGradient]，[RadialGradient]，[SweepGradient] 等..
+  ///
+  /// Sets the gradient background of the component. [BackgroundColor]
+  /// You can choose [LinearGradient], [RadialGradient], [SweepGradient], etc ..
   Gradient gradient;
+  /// 组件背景图片，会覆盖 [backgroundColor]  和 [gradient]。详见 [ImageProvider]
+  ///
+  /// Component background image, overwrites [backgroundColor] and [gradient].
+  /// See [ImageProvider] for details
   ImageProvider backgroundImage;
+  /// 设置组件圆角。详见 [Corner]
+  ///
+  /// Sets the component fillet. See [Corner] for details
   Corner corner;
+  /// 设置圆角风格，默认 [CornerStyle.round]
+  ///
+  /// Set rounded corner style, default [CornerStyle.round]
   CornerStyle cornerStyle;
+  /// 设置组件内间距，只影响文字。通过该属性可以设置文字和 [FSuper.child1]、[FSuper.child2] 的间距。
+  ///
+  /// Sets the spacing between components, affecting only text.
+  /// This property can set the space between the text and [FSuper.child1], [FSuper.child2].
   EdgeInsetsGeometry padding;
+  /// 设置组件与父容器边缘的间距。详见 [EdgeInsets]
+  ///
+  /// Sets the distance between the component and the edge of the parent container. See [EdgeInsets] for details
   EdgeInsets margin;
+  /// 子组件。子组件允许是任意类型的 Widget。
+  /// 通过 [child1Alignment] 和 [child1Margin] 可以控制它在组件中的位置。
+  ///
+  /// Subassembly. Child components are allowed to be any type of widget.
+  /// [Child1Alignment] and [child1Margin] can control its position in the component.
   Widget child1;
+  /// 指定 [child1] 在组件中的相对位置
+  ///
+  /// Specify the relative position of the child component in the component
   Alignment child1Alignment;
+  /// 基于 [child1] 的相对位置，为它设置偏移
+  ///
+  /// Set an offset for the child component based on its relative position
   EdgeInsets child1Margin;
+  /// 为 [child1] 设置点击监听器
+  ///
+  /// Set click listener for [child1]
   GestureTapCallback onChild1Click;
+  /// 子组件。子组件允许是任意类型的 Widget。
+  /// 通过 [child1Alignment] 和 [child1Margin] 可以控制它在组件中的位置。
+  ///
+  /// Subassembly. Child components are allowed to be any type of widget.
+  /// [Child1Alignment] and [child1Margin] can control its position in the component.
   Widget child2;
+  /// 指定 [child2] 在组件中的相对位置
+  ///
+  /// Specify the relative position of the child component in the component
   Alignment child2Alignment;
+  /// 基于 [child2] 的相对位置，为它设置偏移
+  ///
+  /// Set an offset for the child component based on its relative position
   EdgeInsets child2Margin;
+  /// 为 [child2] 设置点击监听器
+  ///
+  /// Set click listener for [child2]
   GestureTapCallback onChild2Click;
+  /// 是否显示小红点。默认不展示。
+  ///
+  /// Whether to show the Red Point.Not displayed by default.
   bool redPoint;
+  /// 小红点颜色。默认 [Colors.redAccent]
+  ///
+  /// Red Point color. Default [Colors.redAccent]
   Color redPointColor;
+  /// 设置 Red Point 大小。Red Point 的 宽=高
+  ///
+  /// Set the Red Point size. Red Point width = height
   double redPointSize;
+  /// 设置 Red Point 中的文字。比如消息数量..
+  ///
+  /// Set the text in Red Point. Like number of messages ...
   String redPointText;
+  /// 设置 Red Point 中文字的颜色。默认 [Colors.white]
+  ///
+  /// Sets the color of text in Red Point. Default [Colors.white]
   Color redPointTextColor;
+  /// 设置 Red Point 中文字的字号。默认 11
+  ///
+  /// Sets the font size of the text in Red Point. Default 11
   double redPointTextSize;
+  /// 基于组件右上角设置 Red Point 的位置偏移。
+  /// 默认 Red Point 会向右上方偏移 [redPointSize] / 2
+  ///
+  /// Sets the position of the Red Point based on the upper right corner of the component.
+  /// The default Red Point is shifted to the upper right [redPointSize] / 2
   Offset redPointOffset;
+  /// 设置边框颜色。
+  ///
+  /// Set the border color.
   Color strokeColor;
+  /// 设置边框宽
+  ///
+  /// Set border width
   double strokeWidth;
+  /// 设置组件阴影颜色
+  ///
+  /// Set component shadow color
   Color shadowColor;
+  /// 设置组件阴影偏移
+  ///
+  /// Set component shadow offset
   Offset shadowOffset;
+  /// 设置组件高斯与阴影形状卷积的标准偏差。
+  ///
+  /// Sets the standard deviation of the component's Gaussian convolution with the shadow shape.
   double shadowBlur;
 
   FSuper({
@@ -102,6 +240,9 @@ class FSuper extends StatefulWidget {
     return _FSuperState();
   }
 
+  /// 在富文本 [spans] 中插入垂直方向的文字间距
+  ///
+  /// Insert vertical spacing in rich text [spans]
   static TextSpan VerticalSpace(double space) {
     return TextSpan(
         text: '\n \n',
@@ -111,6 +252,9 @@ class FSuper extends StatefulWidget {
         ));
   }
 
+  /// 在富文本 [spans] 中插入水平方向的文字间距
+  ///
+  /// Insert horizontal spacing in rich text [spans]
   static TextSpan HorizontalSpace(double space) {
     return TextSpan(
         text: " ",
@@ -125,6 +269,14 @@ class _FSuperState extends State<FSuper> {
   GlobalKey textContainerKey = GlobalKey();
   Size containerSize;
 
+  /// [FSuper] 在初始化时会注册一帧回调，当组件首次测量完成后，将会根据现有尺寸根据再次确定尺寸配置。
+  /// 接着会确定 [FSuper.child1]，[FSuper.child2] 的位置。
+  /// 最后会再次触发绘制。
+  ///
+  /// [FSuper] A frame callback is registered during initialization.
+  /// After the component's first measurement is completed, the size configuration will be determined again based on the existing size.
+  /// Then the location of [FSuper.child1], [FSuper.child2] will be determined.
+  /// Finally, the drawing will be triggered again.
   @override
   void initState() {
 //    print("initState");
@@ -162,7 +314,7 @@ class _FSuperState extends State<FSuper> {
       });
 //      print("_handleSizeChanged.setState");
     }
-    WidgetsBinding.instance.addPostFrameCallback(_handleSizeChanged);
+//    WidgetsBinding.instance.addPostFrameCallback(_handleSizeChanged);
 //    print("_handleSizeChanged");
   }
 
@@ -272,6 +424,9 @@ class _FSuperState extends State<FSuper> {
     return result;
   }
 
+  /// 创建 [FSuper.child1] 和  [FSuper.child2] 部分
+  ///
+  /// Create [FSuper.child1] and [FSuper.child2] part
   SizedBox _buildChildPart() {
     List<_ChildInfo> childInfos = [];
     List<Widget> layoutIds = [];
@@ -348,12 +503,18 @@ class _FSuperState extends State<FSuper> {
   }
 }
 
+/// 为 [FSuper] 设置圆角。详见 [FSuper.corner]
+///
+/// Set rounded corners for [FSuper]. See [FSuper.corner] for details
 class Corner {
   final double leftTopCorner;
   final double rightTopCorner;
   final double rightBottomCorner;
   final double leftBottomCorner;
 
+  /// 指定每一个圆角的大小
+  ///
+  /// Specify the size of each rounded corner
   const Corner({
     this.leftTopCorner = 0,
     this.rightTopCorner = 0,
@@ -361,6 +522,9 @@ class Corner {
     this.leftBottomCorner = 0,
   });
 
+  /// 设置所有圆角为一个大小
+  ///
+  /// Set all rounded corners to one size
   Corner.all(double radius)
       : leftTopCorner = radius,
         rightTopCorner = radius,
@@ -368,6 +532,9 @@ class Corner {
         leftBottomCorner = radius;
 }
 
+/// 确定 [FSuper.child1] 和 [FSuper.child2] 在 [FSuper] 中的位置
+///
+/// Determine where [FSuper.child1] and [FSuper.child2] are in [FSuper]
 class _LayoutDelegate extends MultiChildLayoutDelegate {
   List<_ChildInfo> childInfos;
 
@@ -395,6 +562,9 @@ class _LayoutDelegate extends MultiChildLayoutDelegate {
     }
   }
 
+  /// 在首次测量获得 [FSuper] 的准确大小后，根据 [FSuper.child1] 和 [FSuper.child2] 的配置确定它们的位置
+  ///
+  /// After obtaining the exact size of [FSuper] for the first measurement, position them according to the configuration of [FSuper.child1] and [FSuper.child2]
   void handleChild(_ChildInfo child, Size size) {
     Size childSize = layoutChild(
         child, BoxConstraints(maxWidth: size.width, maxHeight: size.height));
@@ -436,6 +606,11 @@ class _LayoutDelegate extends MultiChildLayoutDelegate {
   }
 }
 
+/// 用于记录 [FSuper.child1] 和 [FSuper.child2] 的信息。
+/// 在进行布局的时候会用到。
+///
+/// Used to record the information of [FSuper.child1] and [FSuper.child2].
+/// will be used during layout.
 class _ChildInfo {
   String type;
   Alignment alignment;
